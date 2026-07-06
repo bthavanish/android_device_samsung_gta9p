0d2711ae702d86cf895fdbf9614aedf2a4f24cc4
bthavanish <bthavanish@users.noreply.github.com>
2026-07-07 01:36:34 +0530

    add device.mk with package inclusions and overlays
    Signed-off-by: Developer <developer@lineageos.org>

---
+++ device.mk
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2024-2025 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/gta9p/gta9p-vendor.mk)
+
+# Local overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.gta9p.rc \
+    init.vendor.rilchip.rc \
+    init.vendor.rilcommon.rc
+
+# IPA
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
+# RIL
+PRODUCT_PACKAGES += \
+    libxml2 \
+    secril_config_svc \
+    vendor.samsung.hardware.radio@2.2
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# WiFi firmware
+PRODUCT_PACKAGES += \
+    dummy_wlanmdsp.mbn
+
+# Inherit from gta9p-common
+$(call inherit-product, device/samsung/gta9p-common/gta9p.mk)
